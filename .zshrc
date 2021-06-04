c19873a633d382a06bc3d3da81a954aeddf2180a
Matthew Ferry <matthewferry@github.com>
2021-06-04 20:34:47 +0000

    Adds initial preferences and apps
    - Adds .zshrc
    - Adds .macos
    - Adds Brewfile
    - Adds .vscode

---
+++ .zshrc
@@ -0,0 +1,32 @@
+# Completion
+autoload -Uz compinit
+compinit
+
+setopt MENU_COMPLETE
+setopt CORRECT
+setopt CORRECT_ALL
+
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
+
+# Stylize prompt with git status
+autoload -Uz add-zsh-hook vcs_info
+add-zsh-hook precmd vcs_info
+
+setopt prompt_subst
+
+zstyle ':vcs_info:*' check-for-changes true # check for changes
+zstyle ':vcs_info:*' unstagedstr '%F{1}*%f' # customize unstaged changes string
+zstyle ':vcs_info:*' stagedstr '%F{2}+%f' # customize staged changes string
+zstyle ':vcs_info:git:*' formats '(%b%u%c)' # set the format for vcs_info
+zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c)' # set the action format, e.g. rebase, for vcs_info
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked # set hook format for untracked changes
+
++vi-git-untracked(){
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+        git status --porcelain | grep '??' &> /dev/null ; then
+
+        hook_com[staged]+='%F{3}?%f'
+    fi
+}
+
+PROMPT='%B%F{7}%~%f%F{12}${vcs_info_msg_0_}%f%b '
---
+++ macos.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+# System Preferences > General > Appearance
+# Set Appearance to "Auto"
+defaults write -g AppleInterfaceStyleSwitchesAutomatically -bool true
+
+# System Preferences > Language & Region > General > Time Format
+# Enforces 24 hour time across all timestamps and clocks
+defaults write -g AppleICUForce24HourTime -bool true
+
+# System Preferences > Dock & Menu Bar > Dock size
+# Set the dock size to small
+defaults write com.apple.dock tilesize -int 42
+
+# System Preferences > Dock & Menu Bar > Dock
+# Set dock to hide/show automatically
+defaults write com.apple.dock autohide -bool true
+
+# System Preferences > Dock & Menu Bar > Dock
+# Set the dock on the left side of the display
+defaults write com.apple.dock orientation -string "left"
+
+# System Preferences > Dock & Menu Bar > Dock
+# Set dock to not display recent apps
+defaults write com.apple.dock show-recents -bool false
+
+# System Preferences > Dock & Menu Bar > Menu Bar
+# Automatically hide/show Menu Bar
+defaults write -g _HIHideMenuBar -bool true
+
+# Safari > View > Hide/Show Favorites Bar
+# Hides Safari’s favorites/bookmarks bar by default
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Safari > Preferences > Advanced
+# Show Developer menu item for developer tools
+defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
+
+# Safari > Preferences > Search > Search engine
+# Set the Safari default search engine to DuckDuckGo
+defaults write -g NSPreferredWebServices -string "{ NSWebServicesProviderWebSearch = { NSDefaultDisplayName = DuckDuckGo; NSProviderIdentifier = \"com.duckduckgo\"; }; }"
+defaults write com.apple.SafariServices SearchProviderIdentifierMigratedToSystemPreference -int 1
+
+# Finder > Preferences > General > Show items on Desktop
+# Shows external drives on Desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+
+# Screencapture > Options > Save to…
+# Changes default location for screencaptures to ~/Documents
+defaults write com.apple.screencapture location -string "$HOME/Documents/"
+
+# Reorders the preferred order of Spotlight results
+# defaults write com.apple.spotlight orderedItems -array \
+#         '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+#         '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+#         '{"enabled" = 1;"name" = "DIRECTORIES";}' \
+#         '{"enabled" = 1;"name" = "PDF";}' \
+#         '{"enabled" = 0;"name" = "FONTS";}' \
+#         '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+#         '{"enabled" = 0;"name" = "MESSAGES";}' \
+#         '{"enabled" = 0;"name" = "CONTACT";}' \
+#         '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+#         '{"enabled" = 0;"name" = "IMAGES";}' \
+#         '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+#         '{"enabled" = 0;"name" = "MUSIC";}' \
+#         '{"enabled" = 0;"name" = "MOVIES";}' \
+#         '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+#         '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+#         '{"enabled" = 0;"name" = "SOURCE";}'
+
+# Terminal > Preferences > Profiles
+# Set default theme to my modified version of Basic with system colors for auto light/dark mode
+open "${PWD}/matthewferry.terminal"
+sleep 1 # Wait to make sure the theme is loaded
+defaults write com.apple.Terminal "Default Window Settings" -string "System color theme"
+defaults write com.apple.Terminal "Startup Window Settings" -string "System color theme"
+
+# Restart affected apps like Dock and Finder
+for app in "Dock" "Finder" "Safari"; do
+  killall "${app}" > /dev/null 2>&1
+done
+
+# Restart for some system changes to take effect
+echo -e "Some of these changes require you to restart.\n"
+echo -ne "\033[1;34mWould you like to restart now? (y/n):\033[0m "
+read -r
+
+shopt -s nocasematch
+if [[ "$REPLY" =~ y[es]? ]]; then
+  for i in {03..01}; do
+    echo -en "\rRestarting in $i..."
+    sleep 1
+  done
+  echo
+  sudo shutdown -r now
+fi
