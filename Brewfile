5bb024efda5ff17fcf09258251df21bc8f2e2e7a
Matthew Ferry <matthewferry@github.com>
2021-06-18 13:03:12 -0600

    Adds initial preferences and apps (#1)
    - Adds .zshrc
    - Adds .macos
    - Adds Brewfile
    - Adds .vscode

---
+++ Brewfile
@@ -0,0 +1,38 @@
+# Install formulae
+brew "clojure"
+brew "gh"
+brew "git"
+brew "java"
+brew "mas"
+brew "nodenv"
+brew "tmux"
+
+# Install apps
+cask "principle"
+cask "1password"
+cask "exodus"
+cask "figma"
+cask "flux"
+cask "kap"
+cask "loom"
+cask "loopback"
+cask "muzzle"
+cask "qlab"
+cask "sim-daltonism"
+cask "slack"
+cask "superhuman"
+cask "tuple"
+cask "visual-studio-code"
+
+# Install App Store stuffs
+mas "AdBlock", id: 1402042596
+mas "Binaural", id: 946812450
+mas "Contrast", id: 1254981365
+mas "Gifski", id: 1351639930
+mas "Keynote", id: 409183694
+mas "Logic Pro", id: 634148309
+mas "MainStage", id: 634159523
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Sprinkles", id: 1500209074
+mas "Xcode", id: 497799835
---
+++ macos.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# System Preferences > General > Appearance
+# Set Appearance to "Auto"
+defaults write -g AppleInterfaceStyleSwitchesAutomatically -bool true
+
+# System Preferences > Language & Region > General > Time Format
+# Enforces 24 hour time across all timestamps and clocks
+defaults write -g AppleICUForce24HourTime -bool true
+
+# System Preferences > Dock & Menu Bar > Dock size
+# Set the dock size to small
+defaults write com.apple.dock tilesize -int 42
+
+# System Preferences > Dock & Menu Bar > Dock
+# Set dock to hide/show automatically
+defaults write com.apple.dock autohide -bool true
+
+# System Preferences > Dock & Menu Bar > Dock
+# Set the dock on the left side of the display
+defaults write com.apple.dock orientation -string "left"
+
+# System Preferences > Dock & Menu Bar > Dock
+# Set dock to not display recent apps
+defaults write com.apple.dock show-recents -bool false
+
+# System Preferences > Dock & Menu Bar > Menu Bar
+# Automatically hide/show Menu Bar
+defaults write -g _HIHideMenuBar -bool true
+
+# Safari > View > Hide/Show Favorites Bar
+# Hides Safari’s favorites/bookmarks bar by default
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Safari > Preferences > Advanced
+# Show Developer menu item for developer tools
+defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
+
+# Safari > Preferences > Search > Search engine
+# Set the Safari default search engine to DuckDuckGo
+defaults write -g NSPreferredWebServices -string "{ NSWebServicesProviderWebSearch = { NSDefaultDisplayName = DuckDuckGo; NSProviderIdentifier = \"com.duckduckgo\"; }; }"
+defaults write com.apple.SafariServices SearchProviderIdentifierMigratedToSystemPreference -int 1
+
+# Screencapture > Options > Save to…
+# Changes default location for screencaptures to ~/Documents
+defaults write com.apple.screencapture location -string "$HOME/Documents/"
+
+# Terminal > Preferences > Profiles
+# Set default theme to my modified version of Basic with system colors for auto light/dark mode
+open "${PWD}/matthewferry.terminal"
+sleep 1 # Wait to make sure the theme is loaded
+defaults write com.apple.Terminal "Default Window Settings" -string "matthewferry"
+defaults write com.apple.Terminal "Startup Window Settings" -string "matthewferry"
+
+# Restart affected apps like Dock and Finder
+for app in "Dock" "Finder" "Safari"; do
+  killall "${app}" > /dev/null 2>&1
+done
+
+# Restart for some system changes to take effect
+echo -e "Some of these changes require you to restart.\n"
+echo -ne "\033[1;34mWould you like to restart now? (y/n):\033[0m "
+read -r
+
+shopt -s nocasematch
+if [[ "$REPLY" =~ y[es]? ]]; then
+  for i in {03..01}; do
+    echo -en "\rRestarting in $i..."
+    sleep 1
+  done
+  echo
+  sudo shutdown -r now
+fi
